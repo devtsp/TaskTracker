9f93b0f64422c792a5b4b8613ff60554acadc10b
Tomas Paez <paeztms@gmail.com>
2023-04-16 18:07:31 -0300

    add task list and task item components

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css'],
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
   @Input() text: string = 'CLICK';
   @Input() backgorundColor: string = 'black';
+  @Output() btnClick = new EventEmitter();
 
-  constructor() {}
-  ngOnInit(): void {}
+  onClick() {
+    this.btnClick.emit();
+  }
 }
