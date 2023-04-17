946b8f4e1446d30d4ca002e50a1088f44b529ec4
Tomas Paez <paeztms@gmail.com>
2023-04-16 22:47:48 -0300

    add json server and hhtp module

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
